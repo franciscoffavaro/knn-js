fac94220f1735262c1182aa8b534a63a91863029
Francisco Favaro <franciscoffavaro@gmail.com>
2017-05-20 23:42:21 -0300

    Same.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 "use strict"
-
 const fs = require('fs');
-const csv = require('fast-csv');
+const math = require('mathjs');
+
+class Rose {
+    constructor(sepallength_, sepalwidth_, petallength_, petalwidth_, label_) {
+        let sepallength = sepallength_;
+        let sepalwidth = sepalwidth_;
+        let petallength = petallength_;
+        let petalwidth = petalwidth_;
+        let label = label_;
+
+        this.getSepallength = () => sepallength;
+        this.getSepalwidth = () => sepalwidth;
+        this.getPetallength = () => petallength;
+        this.getPetalwidth = () => petalwidth;
+        this.getLabel = () => label;
+
+        this.setSepallength = (a_) => sepallength = a_;
+        this.setSepalwidth = (a_) => sepalwidth = a_;
+        this.setPetallength = (a_) => petallength = a_;
+        this.setPetalwidth = (a_) => petallength = a_;
+        this.setLabel = (a_) => label = a_;
 
 
-
-const readCsv = (path) => {
-    let array = []
-    fs.createReadStream(path)
-        .pipe(csv())
-        .on('data', (data) => {
-            console.log(data)
-        });
+    }
 }
 
-readCsv('../files/treinamento.csv')
+const readCsvFile = (path) => fs.readFileSync(path, 'utf8').split('\n');
+
+const euclidianDistance = (rose1, rose2) => {
+    const soma = math.pow((rose1.getSepallength() - rose2.getSepallength()), 2) +
+        math.pow((rose1.getSepalwidth() - rose2.getSepalwidth()), 2) +
+        math.pow((rose1.getPetallength() - rose2.getPetallength()), 2) +
+        math.pow((rose1.getPetalwidth() - rose2.getPetalwidth()), 2);
+    return math.sqrt(soma);
+}
